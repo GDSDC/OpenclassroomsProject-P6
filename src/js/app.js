258b5d61a7932411bd109fe9cca860f932716901
GDSDC <gdc.business.2019@gmail.com>
2022-03-24 09:57:02 +0100

    fix(formatting) : formatting script

--- src/js/app.js
+++ src/js/app.js
@@ -1,45 +1,14 @@
-var movies_ids = [
-    "best_movie_IMG",
-    "top_rated_movie_1",
-    "top_rated_movie_2",
-    "top_rated_movie_3",
-    "top_rated_movie_4",
-    "top_rated_movie_5",
-    "top_rated_movie_6",
-    "top_rated_movie_7",
-    "category_1_movie_1",
-    "category_1_movie_2",
-    "category_1_movie_3",
-    "category_1_movie_4",
-    "category_1_movie_5",
-    "category_1_movie_6",
-    "category_1_movie_7",
-    "category_2_movie_1",
-    "category_2_movie_2",
-    "category_2_movie_3",
-    "category_2_movie_4",
-    "category_2_movie_5",
-    "category_2_movie_6",
-    "category_2_movie_7",
-    "category_3_movie_1",
-    "category_3_movie_2",
-    "category_3_movie_3",
-    "category_3_movie_4",
-    "category_3_movie_5",
-    "category_3_movie_6",
-    "category_3_movie_7"]
+const movies_ids = ["best_movie_IMG", "top_rated_movie_1", "top_rated_movie_2", "top_rated_movie_3", "top_rated_movie_4", "top_rated_movie_5", "top_rated_movie_6", "top_rated_movie_7", "category_1_movie_1", "category_1_movie_2", "category_1_movie_3", "category_1_movie_4", "category_1_movie_5", "category_1_movie_6", "category_1_movie_7", "category_2_movie_1", "category_2_movie_2", "category_2_movie_3", "category_2_movie_4", "category_2_movie_5", "category_2_movie_6", "category_2_movie_7", "category_3_movie_1", "category_3_movie_2", "category_3_movie_3", "category_3_movie_4", "category_3_movie_5", "category_3_movie_6", "category_3_movie_7"]
 
 
 function get_movie_image(id) {
-    var movie_img = document.createElement("img");
+    let movie_img = document.createElement("img");
     movie_img.src = "src/tests/img/PNG/" + id + ".png";
     movie_img.className = "thumbnail"
     return movie_img;
 }
 
 // Display Images
-for (var i = 0; i < movies_ids.length; i++) {
+for (let i = 0; i < movies_ids.length; i++) {
     document.getElementById(movies_ids[i]).append(get_movie_image(movies_ids[i]));
 }
-
-// console.log(movies_ids[0])
