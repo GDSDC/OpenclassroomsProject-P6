1eef59ad40bff0696b422104d95bba92c0822e35
GDSDC <gdc.business.2019@gmail.com>
2022-04-01 12:09:49 +0200

    fix(modal) : reorganising code so close button work well to close modal

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,6 @@
 const movies_ids = ["best_movie", "top_rated_movie_1", "top_rated_movie_2", "top_rated_movie_3", "top_rated_movie_4", "top_rated_movie_5", "top_rated_movie_6", "top_rated_movie_7", "category_1_movie_1", "category_1_movie_2", "category_1_movie_3", "category_1_movie_4", "category_1_movie_5", "category_1_movie_6", "category_1_movie_7", "category_2_movie_1", "category_2_movie_2", "category_2_movie_3", "category_2_movie_4", "category_2_movie_5", "category_2_movie_6", "category_2_movie_7", "category_3_movie_1", "category_3_movie_2", "category_3_movie_3", "category_3_movie_4", "category_3_movie_5", "category_3_movie_6", "category_3_movie_7"]
 
 
-
 function get_movies_images_json() {
     return fetch("src/tests/img/JSON/movies_images.json")
         .then(response => {
@@ -19,35 +18,58 @@ function get_movie_image(id) {
 }
 
 
-// // Display Images
-// for (let i = 0; i < movies_ids.length; i++) {
-//     document.getElementById(movies_ids[i]).append(get_movie_image(movies_ids[i]));
+// const xhr = new XMLHttpRequest();
+// xhr.open("GET", "http://localhost:8000/api/v1/genres/");
+// xhr.send()
+//
+// xhr.onload = function () {
+//     if (xhr.status === 200) {
+//         data = JSON.parse(xhr.responseText);
+//         console.log(data.count);
+//         console.log(data.products);
+//     } else if (xhr.status === 404) {
+//         console.log("No records found")
+//     }
 // }
-
-const xhr = new XMLHttpRequest();
-xhr.open("GET", "http://localhost:8000/api/v1/genres/");
-xhr.send()
-
-xhr.onload = function () {
-    if (xhr.status === 200) {
-        data = JSON.parse(xhr.responseText);
-        console.log(data.count);
-        console.log(data.products);
-    } else if (xhr.status === 404) {
-        console.log("No records found")
-    }
-}
 
 
 // Get the modal
 var modal_elements = document.querySelectorAll(".modal");
+
+// Generate HTML modal template
+function generate_modal_HTML(id) {
+    let modal_HTML_output = '<!-- Modal content -->\n' +
+        '                <div class="modal-content">\n' +
+        '                    <span class="close">&times;</span>\n' +
+        '                    <img class="modal__movie__thumbnail" src="assets/' + movies_ids[id] + '_placeholder.png"/>\n' +
+        '                    <p class="movie__title">Titre : ' + movies_ids[id] + '_TITLE_placeholder</p>\n' +
+        '                    <p class="movie__genres">Genre Complet : ' + movies_ids[id] + '_GENRES_placeholder</p>\n' +
+        '                    <p class="movie__year">Date de sortie : ' + movies_ids[id] + '_YEAR_placeholder</p>\n' +
+        '                    <p class="movie__rated">Rated : ' + movies_ids[id] + '_RATED_placeholder</p>\n' +
+        '                    <p class="movie__imdb_score">Rated : ' + movies_ids[id] + '_IMDB_SCORE_placeholder</p>\n' +
+        '                    <p class="movie__directors">Réalisateur : ' + movies_ids[id] + '_DIRECTORS_placeholder</p>\n' +
+        '                    <p class="movie__actors">Liste des acteurs : ' + movies_ids[id] + '_ACTORS_placeholder</p>\n' +
+        '                    <p class="movie__duration">Durée : ' + movies_ids[id] + '_DURATION_placeholder</p>\n' +
+        '                    <p class="movie__country">Pays d\'origine : ' + movies_ids[id] + '_COUNTRY_placeholder</p>\n' +
+        '                    <p class="movie__box_office_score">Résultat au Box Office : ' + movies_ids[id] + '_BOX_OFFICE_SCORE_placeholder</p>\n' +
+        '                    <p class="movie__summary">Résumé du film : ' + movies_ids[id] + '_SUMMARY_placeholder</p>\n' +
+        '                </div>\n' +
+        '            </div>'
+    return modal_HTML_output
+}
+
+// Inserting all modals in HTML
+for (let i = 0; i < modal_elements.length; i++) {
+    modal_elements[i].innerHTML = generate_modal_HTML(i);
+}
+
 
 // Get the button that opens the modal
 var modal__trigger_elements = document.querySelectorAll(".modal__trigger");
 
 // Get the <span> element that closes the modal
 var span_elements = document.querySelectorAll(".close");
-console.log(span_elements.length)
+console.log(span_elements.length);
 
 // When the user clicks on the modal__triggers, open the modal
 for (let i = 0; i < modal__trigger_elements.length; i++) {
@@ -78,29 +100,3 @@ window.addEventListener('keydown', function (event) {
     }
 })
 
-// Generate HTML modal template
-function generate_modal_HTML (id){
-    let modal_HTML_output = '<!-- Modal content -->\n' +
-        '                <div class="modal-content">\n' +
-        '                    <span class="close">&times;</span>\n' +
-        '                    <img class="modal__movie__thumbnail" src="assets/' + movies_ids[id] + '_placeholder.png"/>\n' +
-        '                    <p class="movie__title">Titre : ' + movies_ids[id] + '_TITLE_placeholder</p>\n' +
-        '                    <p class="movie__genres">Genre Complet : ' + movies_ids[id] + '_GENRES_placeholder</p>\n' +
-        '                    <p class="movie__year">Date de sortie : ' + movies_ids[id] + '_YEAR_placeholder</p>\n' +
-        '                    <p class="movie__rated">Rated : ' + movies_ids[id] + '_RATED_placeholder</p>\n' +
-        '                    <p class="movie__imdb_score">Rated : ' + movies_ids[id] + '_IMDB_SCORE_placeholder</p>\n' +
-        '                    <p class="movie__directors">Réalisateur : ' + movies_ids[id] + '_DIRECTORS_placeholder</p>\n' +
-        '                    <p class="movie__actors">Liste des acteurs : ' + movies_ids[id] + '_ACTORS_placeholder</p>\n' +
-        '                    <p class="movie__duration">Durée : ' + movies_ids[id] + '_DURATION_placeholder</p>\n' +
-        '                    <p class="movie__country">Pays d\'origine : ' + movies_ids[id] + '_COUNTRY_placeholder</p>\n' +
-        '                    <p class="movie__box_office_score">Résultat au Box Office : ' + movies_ids[id] + '_BOX_OFFICE_SCORE_placeholder</p>\n' +
-        '                    <p class="movie__summary">Résumé du film : ' + movies_ids[id] + '_SUMMARY_placeholder</p>\n' +
-        '                </div>\n' +
-        '            </div>'
-    return modal_HTML_output
-}
-
-// Inserting all modals in HTML
-    for (let i = 0; i < modal_elements.length; i++) {
-        modal_elements[i].innerHTML = generate_modal_HTML (i);
-    }
