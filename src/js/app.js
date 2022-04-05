dfb5a5ba421b3aed07975f1d031031c813e1bf8f
GDSDC <gdc.business.2019@gmail.com>
2022-04-05 10:34:13 +0200

    build(window.onload get_movie_details update_modal_content) : update update_modal_content to fill in content in modal / create get_movie_details to get all details for a single movie / create window.onload to wait until the page is loaded to proceed

--- src/js/app.js
+++ src/js/app.js
@@ -89,55 +89,420 @@ function get_movies_filtered(filter_input) {
     return movies_filtered;
 }
 
+function get_movie_details(movie_id) {
+// Initiate Request
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://localhost:8000/api/v1/titles/" + movie_id);
+    xhr.send();
+
+// Initiate variables
+    let movie_details = [];
+
+// Request Onload
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            // Fill in movie details
+            movie_details.push(xhr.responseText);
+        } else if (xhr.status === 404) {
+            console.log("No records found")
+        }
+    }
+    return movie_details[0];
+}
+
 movies_action = get_movies_filtered('imdb_score_min=8.8&genre=Action');
 movies_musical = get_movies_filtered('imdb_score_min=8.8&genre=Musical');
 movies_trhiller = get_movies_filtered('imdb_score_min=8.8&genre=Thriller');
 best_rated_movies = get_movies_filtered('imdb_score_min=9');
 
-delay(500).then(() => console.log(movies_action));
-delay(500).then(() => console.log(movies_musical));
-delay(500).then(() => console.log(movies_trhiller));
-delay(250).then(() => console.log(best_rated_movies));
+const category_movies_test = [
+    {
+        "id": 50083,
+        "url": "http://localhost:8000/api/v1/titles/50083",
+        "imdb_url": "https://www.imdb.com/title/tt0050083/",
+        "title": "La parola ai giurati",
+        "year": 1957,
+        "imdb_score": "9.0",
+        "votes": 681074,
+        "image_url": "https://m.media-amazon.com/images/M/MV5BMWU4N2FjNzYtNTVkNC00NzQ0LTg0MjAtYTJlMjFhNGUxZDFmXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_UX182_CR0,0,182,268_AL_.jpg",
+        "directors": [
+            "Sidney Lumet"
+        ],
+        "actors": [
+            "E.G. Marshall",
+            "Ed Begley",
+            "Edward Binns",
+            "George Voskovec",
+            "Henry Fonda",
+            "Jack Klugman",
+            "Jack Warden",
+            "John Fiedler",
+            "Joseph Sweeney",
+            "Lee J. Cobb",
+            "Martin Balsam",
+            "Robert Webber"
+        ],
+        "writers": [
+            "Reginald Rose"
+        ],
+        "genres": [
+            "Crime",
+            "Drama"
+        ]
+    },
+    {
+        "id": 68646,
+        "url": "http://localhost:8000/api/v1/titles/68646",
+        "imdb_url": "https://www.imdb.com/title/tt0068646/",
+        "title": "Il padrino",
+        "year": 1972,
+        "imdb_score": "9.2",
+        "votes": 1598655,
+        "image_url": "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY268_CR3,0,182,268_AL_.jpg",
+        "directors": [
+            "Francis Ford Coppola"
+        ],
+        "actors": [
+            "Abe Vigoda",
+            "Al Lettieri",
+            "Al Pacino",
+            "Diane Keaton",
+            "Gianni Russo",
+            "James Caan",
+            "John Cazale",
+            "John Marley",
+            "Marlon Brando",
+            "Richard Conte",
+            "Richard S. Castellano",
+            "Robert Duvall",
+            "Rudy Bond",
+            "Sterling Hayden",
+            "Talia Shire"
+        ],
+        "writers": [
+            "Francis Ford Coppola",
+            "Mario Puzo"
+        ],
+        "genres": [
+            "Crime",
+            "Drama"
+        ]
+    },
+    {
+        "id": 71562,
+        "url": "http://localhost:8000/api/v1/titles/71562",
+        "imdb_url": "https://www.imdb.com/title/tt0071562/",
+        "title": "Il padrino - Parte II",
+        "year": 1974,
+        "imdb_score": "9.0",
+        "votes": 1116614,
+        "image_url": "https://m.media-amazon.com/images/M/MV5BMWMwMGQzZTItY2JlNC00OWZiLWIyMDctNDk2ZDQ2YjRjMWQ0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY268_CR3,0,182,268_AL_.jpg",
+        "directors": [
+            "Francis Ford Coppola"
+        ],
+        "actors": [
+            "Al Pacino",
+            "Bruno Kirby",
+            "Diane Keaton",
+            "Francesca De Sapio",
+            "Frank Sivero",
+            "G.D. Spradlin",
+            "Gastone Moschin",
+            "John Cazale",
+            "Lee Strasberg",
+            "Michael V. Gazzo",
+            "Richard Bright",
+            "Robert De Niro",
+            "Robert Duvall",
+            "Talia Shire",
+            "Tom Rosqui"
+        ],
+        "writers": [
+            "Francis Ford Coppola",
+            "Mario Puzo"
+        ],
+        "genres": [
+            "Crime",
+            "Drama"
+        ]
+    },
+    {
+        "id": 84302,
+        "url": "http://localhost:8000/api/v1/titles/84302",
+        "imdb_url": "https://www.imdb.com/title/tt0084302/",
+        "title": "Maratonci trce pocasni krug",
+        "year": 1982,
+        "imdb_score": "9.0",
+        "votes": 14295,
+        "image_url": "https://m.media-amazon.com/images/M/MV5BZmE4Njk2MWYtZDRlMy00OTAxLThlNWYtOTU3MGEwNTY4Nzc0XkEyXkFqcGdeQXVyMTAwNjUxNTkz._V1_UY268_CR6,0,182,268_AL_.jpg",
+        "directors": [
+            "Slobodan Sijan"
+        ],
+        "actors": [
+            "Bogdan Diklic",
+            "Bora Todorovic",
+            "Danilo 'Bata' Stojkovic",
+            "Dragoljub Milosavljevic-Gula",
+            "Fahro Konjhodzic",
+            "Jelisaveta 'Seka' Sablic",
+            "Melita Bihali",
+            "Mihajlo 'Bata' Paskaljevic",
+            "Mija Aleksic",
+            "Milivoje 'Mica' Tomic",
+            "Miroslav Jovanovic",
+            "Pavle Vuisic",
+            "Radislav Lazarevic",
+            "Veljko Mandic",
+            "Zoran Radmilovic"
+        ],
+        "writers": [
+            "Dusan Kovacevic"
+        ],
+        "genres": [
+            "Comedy",
+            "Drama"
+        ]
+    },
+    {
+        "id": 111161,
+        "url": "http://localhost:8000/api/v1/titles/111161",
+        "imdb_url": "https://www.imdb.com/title/tt0111161/",
+        "title": "Le ali della libertà",
+        "year": 1994,
+        "imdb_score": "9.3",
+        "votes": 2315277,
+        "image_url": "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg",
+        "directors": [
+            "Frank Darabont"
+        ],
+        "actors": [
+            "Bob Gunton",
+            "Brian Libby",
+            "Clancy Brown",
+            "David Proval",
+            "Gil Bellows",
+            "James Whitmore",
+            "Jeffrey DeMunn",
+            "Joseph Ragno",
+            "Jude Ciccolella",
+            "Larry Brandenburg",
+            "Mark Rolston",
+            "Morgan Freeman",
+            "Neil Giuntoli",
+            "Tim Robbins",
+            "William Sadler"
+        ],
+        "writers": [
+            "Frank Darabont",
+            "Stephen King"
+        ],
+        "genres": [
+            "Drama"
+        ]
+    },
+    {
+        "id": 170783,
+        "url": "http://localhost:8000/api/v1/titles/170783",
+        "imdb_url": "https://www.imdb.com/title/tt0170783/",
+        "title": "Vchera",
+        "year": 1988,
+        "imdb_score": "9.0",
+        "votes": 3110,
+        "image_url": "https://m.media-amazon.com/images/M/MV5BNzYzMjcyMzA4NF5BMl5BanBnXkFtZTgwMjY5ODIxNjE@._V1_UY268_CR5,0,182,268_AL_.jpg",
+        "directors": [
+            "Ivan Andonov"
+        ],
+        "actors": [
+            "Boris Lukanov",
+            "Christine Bartlet",
+            "Dimitar Goranov",
+            "Georgi Rusev",
+            "Georgi Staykov",
+            "Hristo Shopov",
+            "Kosta Tsonev",
+            "Krasimir Rankov",
+            "Maria Stefanova",
+            "Nikola Rudarov",
+            "Pavel Popandov",
+            "Petar Popyordanov",
+            "Sofiya Kuzeva",
+            "Stoyan Aleksiev",
+            "Svetla Todorova"
+        ],
+        "writers": [
+            "Vlado Daverov"
+        ],
+        "genres": [
+            "Drama"
+        ]
+    },
+    {
+        "id": 247025,
+        "url": "http://localhost:8000/api/v1/titles/247025",
+        "imdb_url": "https://www.imdb.com/title/tt0247025/",
+        "title": "Voynata na taralezhite",
+        "year": 1979,
+        "imdb_score": "9.0",
+        "votes": 759,
+        "image_url": "https://m.media-amazon.com/images/M/MV5BMjEwMDIzOTc1NV5BMl5BanBnXkFtZTgwNDA0MDQxNjE@._V1_UX182_CR0,0,182,268_AL_.jpg",
+        "directors": [
+            "Ivanka Grybcheva"
+        ],
+        "actors": [
+            "Angel Lambev",
+            "Asen Kotsev",
+            "Dimitar Ganev",
+            "Dimitr Dimitrov",
+            "Greta Gancheva",
+            "Kliment Denchev",
+            "Lyudmila Cheshmedzhieva",
+            "Moris Assa",
+            "Ognian Zheliazkov",
+            "Stefan Danailov",
+            "Stoycho Mazgalov",
+            "Tzvetana Maneva",
+            "Valcho Kamarashev",
+            "Vassil Mihajlov",
+            "Yevgeniya Bozhikova"
+        ],
+        "writers": [
+            "Bratya Mormarevi",
+            "Miryana Basheva"
+        ],
+        "genres": [
+            "Comedy"
+        ]
+    }
+];
 
-const test =  {
-            "id": 9,
-            "url": "http://localhost:8000/api/v1/titles/9",
-            "imdb_url": "https://www.imdb.com/title/tt0000009/",
-            "title": "Miss Jerry",
-            "year": 1894,
-            "imdb_score": "6.0",
-            "votes": 154,
-            "image_url": "https://m.media-amazon.com/images/M/MV5BNjA5Y2ZhYzctNDc1Yy00OGViLWI3NGUtOTYwZmE3NDFiYmIxXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_UY268_CR72,0,182,268_AL_.jpg",
-            "directors": [
-                "Alexander Black"
-            ],
-            "actors": [
-                "Blanche Bayliss",
-                "Chauncey Depew",
-                "William Courtenay"
-            ],
-            "writers": [
-                "Alexander Black"
-            ],
-            "genres": [
-                "Romance"
-            ]
-        };
+const movie_details_test = {
+    "id": 50083,
+    "url": "http://localhost:8000/api/v1/titles/50083",
+    "title": "La parola ai giurati",
+    "original_title": "12 Angry Men",
+    "year": 1957,
+    "date_published": "1957-09-04",
+    "duration": 96,
+    "description": "A jury holdout attempts to prevent a miscarriage of justice by forcing his colleagues to reconsider the evidence.",
+    "long_description": "The defense and the prosecution have rested and the jury is filing into the jury room to decide if a young man is guilty or innocent of murdering his father. What begins as an open-and-shut case of murder soon becomes a detective story that presents a succession of clues creating doubt, and a mini-drama of each of the jurors' prejudices and preconceptions about the trial, the accused, and each other. Based on the play, all of the action takes place on the stage of the jury room.",
+    "avg_vote": "8.9",
+    "imdb_score": "9.0",
+    "votes": 681074,
+    "metascore": "96.0",
+    "budget": 350000,
+    "budget_currency": "USD",
+    "usa_gross_income": null,
+    "worldwide_gross_income": 576,
+    "reviews_from_users": 1605,
+    "reviews_from_critics": 149,
+    "image_url": "https://m.media-amazon.com/images/M/MV5BMWU4N2FjNzYtNTVkNC00NzQ0LTg0MjAtYTJlMjFhNGUxZDFmXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_UX182_CR0,0,182,268_AL_.jpg",
+    "actors": [
+        "E.G. Marshall",
+        "Ed Begley",
+        "Edward Binns",
+        "George Voskovec",
+        "Henry Fonda",
+        "Jack Klugman",
+        "Jack Warden",
+        "John Fiedler",
+        "Joseph Sweeney",
+        "Lee J. Cobb",
+        "Martin Balsam",
+        "Robert Webber"
+    ],
+    "directors": [
+        "Sidney Lumet"
+    ],
+    "writers": [
+        "Reginald Rose"
+    ],
+    "genres": [
+        "Crime",
+        "Drama"
+    ],
+    "countries": [
+        "USA"
+    ],
+    "languages": [
+        "English"
+    ],
+    "rated": "Not rated or unkown rating",
+    "company": "Orion-Nova Productions"
+};
+
+var best_rated_movies_details = [];
 
 
-var section_test = document.getElementById('top_rated_movies_section');
-var modal_content_test = section_test.getElementsByClassName('modal-content')[0];
-delay(1000).then(() => update_modal_content(modal_content_test,test2));
+window.onload = function () {
+
+    // for (let i = 0; i < 7; i++) {
+    //     best_rated_movies_details.push(get_movie_details(category_movies_test[i].id));
+    // }
+
+    console.log(movies_action);
+    console.log(movies_musical);
+    console.log(movies_trhiller);
+    console.log(best_rated_movies);
+    console.log(best_rated_movies_details);
+
+    var section_test = document.getElementById('top_rated_movies_section');
+    var modal_content_test = section_test.getElementsByClassName('modal-content');
+    update_modal_content(modal_content_test[0], movie_details_test);
+
+    // for (let i = 0; i < modal_content_test.length; i++) {
+    //     update_modal_content(modal_content_test[i], best_rated_movies_details[i]);
+    // }
+}
 
 
-function update_modal_content(modal_content_element,movie_data){
+function update_modal_content(modal_content_element, movie_data) {
     // movie thumbnail
-    let modal__movie__thumbnail_element = movie_data.getElementsByClassName('modal__movie__thumbnail');
+    let modal__movie__thumbnail_element = modal_content_element.getElementsByClassName('modal__movie__thumbnail')[0];
     modal__movie__thumbnail_element.src = movie_data.image_url;
 
     // movie__title
-    let movie__title_element = movie_data.getElementsByClassName('movie__title');
+    let movie__title_element = modal_content_element.getElementsByClassName('movie__title')[0];
     movie__title_element.textContent = 'Titre : ' + movie_data.title;
+
+    // movie__genres
+    let movie__genres_element = modal_content_element.getElementsByClassName('movie__genres')[0];
+    movie__genres_element.textContent = 'Genre Complet : ' + movie_data.genres;
+
+    // movie__year
+    let movie__year_element = modal_content_element.getElementsByClassName('movie__year')[0];
+    movie__year_element.textContent = 'Date de sortie : ' + movie_data.year;
+
+    // movie__rated
+    let movie__rated_element = modal_content_element.getElementsByClassName('movie__rated')[0];
+    movie__rated_element.textContent = 'Rated : ' + movie_data.rated;
+
+    // movie__imdb_score
+    let movie__imdb_score_element = modal_content_element.getElementsByClassName('movie__imdb_score')[0];
+    movie__imdb_score_element.textContent = 'Score Imdb : ' + movie_data.imdb_score;
+
+    // movie__directors
+    let movie__directors_element = modal_content_element.getElementsByClassName('movie__directors')[0];
+    movie__directors_element.textContent = 'Réalisateur : ' + movie_data.directors;
+
+    // movie__actors
+    let movie__actors_element = modal_content_element.getElementsByClassName('movie__actors')[0];
+    movie__actors_element.textContent = 'Liste des acteurs : ' + movie_data.actors;
+
+    // movie__duration
+    let movie__duration_element = modal_content_element.getElementsByClassName('movie__duration')[0];
+    movie__duration_element.textContent = 'Durée :  ' + movie_data.duration + ' minutes';
+
+    // movie__country
+    let movie__country_element = modal_content_element.getElementsByClassName('movie__country')[0];
+    movie__country_element.textContent = 'Pays d\'origine : ' + movie_data.countries;
+
+    // movie__box_office_score
+    let movie__box_office_score_element = modal_content_element.getElementsByClassName('movie__box_office_score')[0];
+    movie__box_office_score_element.textContent = 'Résultat au Box Office : ' + movie_data.worldwide_gross_income + ' $';
+
+    // movie__summary
+    let movie__summary_element = modal_content_element.getElementsByClassName('movie__summary')[0];
+    movie__summary_element.textContent = 'Résumé du film : ' + movie_data.long_description;
 }
 
 // Get the modal
